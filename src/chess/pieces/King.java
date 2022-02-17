4352d6d5dfa7b74ea0fc2cc97426be6e69e3dcf2
wpmello <wagnermello903@gmail.com>
2022-02-17 14:18:17 -0300

    Implementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,6 +18,65 @@ public class King extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+//        Para cima
+        p.setValues(position.getRow() -1, position.getColumn());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+//        Para cima
+        p.setValues(position.getRow() +1, position.getColumn());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+
+//        Para esquerda
+        p.setValues(position.getRow(), position.getColumn() -1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+//        Para direita
+        p.setValues(position.getRow(), position.getColumn() +1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+
+//        Para diagonal esquerda-cima
+        p.setValues(position.getRow() -1, position.getColumn() -1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+
+//        Para diagonal direita-cima
+        p.setValues(position.getRow() -1, position.getColumn() +1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+
+//        Para diagonal direita-baixo
+        p.setValues(position.getRow() +1, position.getColumn() -1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+//        Para diagonal esquerda-baixo
+        p.setValues(position.getRow() +1, position.getColumn() +1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
         return mat;
+    }
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
     }
 }
